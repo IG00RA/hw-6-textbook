0611bb045c1f5b2d8e5bfb680cf01c9c2c8d6785
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-06-03 09:43:25 +0300

    update to Redux Toolkit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,12 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
-import { rootReducer } from './reducer';
-
-const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+import { configureStore } from '@reduxjs/toolkit';
+//=============== Before ========================
+// import { tasksReducer, filtersReducer } from "./reducer";
+//=============== After ========================
+import { tasksReducer } from './tasksSlice';
+import { filtersReducer } from './filtersSlice';
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    filters: filtersReducer,
+  },
+});
